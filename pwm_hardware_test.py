d146461fbfb6914d52464260caa7af02418401ce
gsyxuexi <gsyxeuxi@gmail.com>
2023-08-11 16:49:50 +0200

    pwm success

--- pwm_hardware_test.py
+++ pwm_hardware_test.py
@@ -11,12 +11,12 @@ def main():
     p1 = GPIO.PWM(32, 1000)
     GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
     p2 = GPIO.PWM(33, 1000)
-    val = 100
-    incr = 5
+    val = 0
+    incr = 10
     p1.start(val)
-    p2.start(val)
+    p2.start(100)
 
-    print("PWM running. Press CTRL+C to exit.")
+    # print("PWM running. Press CTRL+C to exit.")
     # try:
     #     while True:
     #         time.sleep(0.25)
@@ -33,10 +33,27 @@ def main():
     #     GPIO.cleanup()
     
     
-    
-    time.sleep(20)
+    # try:
+    #     while True:
+    #         time.sleep(200)
+    # finally:
+    #     p1.stop()
+    #     p2.stop()
+    #     GPIO.cleanup()
+
+
+
+    while True:
+        time.sleep(1)
+        val2 = 100-val
+        if val == 100:
+            break
+        val += incr
+        p1.ChangeDutyCycle(val)
+        p2.ChangeDutyCycle(val2)
     p1.stop()
     p2.stop()
     GPIO.cleanup()
+
 if __name__ == '__main__':
     main()
