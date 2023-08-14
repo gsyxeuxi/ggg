9c96445b1c231f46b9549ab84f2847537739c431
gsyxuexi <gsyxeuxi@gmail.com>
2023-08-14 13:48:59 +0200

    new

--- pwm_hardware_test.py
+++ pwm_hardware_test.py
@@ -11,26 +11,33 @@ def main():
     p1 = GPIO.PWM(32, 1000)
     GPIO.setup(33, GPIO.OUT, initial=GPIO.HIGH)
     p2 = GPIO.PWM(33, 1000)
-    val = 0
-    incr = 10
-    p1.start(val)
-    p2.start(100)
+    val_1 = 0
+    val_2 = 100
+    incr_1 = 10
+    incr_2 = 10
+    p1.start(val_1)
+    p2.start(val_2)
 
-    # print("PWM running. Press CTRL+C to exit.")
-    # try:
-    #     while True:
-    #         time.sleep(0.25)
-    #         if val >= 100:
-    #             incr = -incr
-    #         if val <= 0:
-    #             incr = -incr
-    #         val += incr
-    #         p1.ChangeDutyCycle(val)
-    #         p2.ChangeDutyCycle(val)
-    # finally:
-    #     p1.stop()
-    #     p2.stop()
-    #     GPIO.cleanup()
+    print("PWM running. Press CTRL+C to exit.")
+    try:
+        while True:
+            time.sleep(0.5)
+            if val_1 >= 100:
+                incr_1 = -incr_1
+            if val_2 >= 100:
+                incr_2 = -incr_2
+            if val_1 <= 0:
+                incr_1 = -incr_1
+            if val_2 <= 0:
+                incr_2= -incr_2
+            val_1 += incr_1
+            val_2 += incr_2
+            p1.ChangeDutyCycle(val_1)
+            p2.ChangeDutyCycle(val_2)
+    finally:
+        p1.stop()
+        p2.stop()
+        GPIO.cleanup()
     
     
     # try:
@@ -41,19 +48,17 @@ def main():
     #     p2.stop()
     #     GPIO.cleanup()
 
-
-
-    while True:
-        time.sleep(1)
-        val2 = 100-val
-        if val == 100:
-            break
-        val += incr
-        p1.ChangeDutyCycle(val)
-        p2.ChangeDutyCycle(val2)
-    p1.stop()
-    p2.stop()
-    GPIO.cleanup()
+    # while True:
+    #     time.sleep(1)
+    #     val2 = 100-val
+    #     if val == 100:
+    #         break
+    #     val += incr
+    #     p1.ChangeDutyCycle(val)
+    #     p2.ChangeDutyCycle(val2)
+    # p1.stop()
+    # p2.stop()
+    # GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
