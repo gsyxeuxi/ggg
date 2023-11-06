e561e7e5f6287c96ed1f6245627e7c5b73199fdf
“YourName” <YourEmail@YourAddress.com>
2023-11-06 17:30:27 +0100

    new

--- rl_test/check.py
+++
@@ -1,8 +0,0 @@
-import h5py
-import numpy as np
-import tensorrt
-
-print(tensorrt.__version__)
-print(np.__version__)
-print(h5py.__version__)
-
--- rl_test/tensorflow_check.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-
-assert tf.config.list_physical_devices('GPU')
-assert tf.test.is_built_with_cuda()
